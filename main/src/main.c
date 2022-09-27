b8fa5437fd118da0ba403687ba29b4e4cd860243
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-09-26 23:48:51 -0700

    Add in basic init structure

--- main/src/main.c
+++ main/src/main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "wifi_cmds.h"
 
 void app_main(void)
 {
+    //Initalize wifi
+    if(wifi_init_sta())
+    {
+        ESP_LOGE("main", "Failed to initialize wifi");
+    }
+    else
+    {
+        ESP_LOGI("main", "Wifi initialized");
+    }
+
+    //Initalize CAN
+    can_init();
+
     while(true)
     {
+    
     }
 
 }
